c49085f2dd44957d638ebac9e2706b0dd96304b2
Jorge Minhalma <jorgeminhalma0@gmail.com>
2020-08-24 16:57:25 +0100

    ComboBox adicionada, MIDAO a funcionar

--- Animex/src/pt/iade/animex/controllers/MIController.java
+++ Animex/src/pt/iade/animex/controllers/MIController.java
@@ -1,52 +1,87 @@
 package pt.iade.animex.controllers;
 
+import java.awt.image.BufferedImage;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
-
 import javax.imageio.ImageIO;
-
-import javafx.embed.swing.SwingFXUtils;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
-import javafx.scene.image.Image;
 import javafx.stage.FileChooser;
 import pt.iade.animex.WindowManager;
+import pt.iade.animex.models.AnimeGenre;
 import pt.iade.animex.models.Dialog;
 import pt.iade.animex.models.daos.MIDAO;
 
 
 
 
-
-
 public class MIController {
-	@FXML
-	private void initialize() {
-
+    private BufferedImage bufferedImage;
+    
+    @FXML
+	private void initialize() { 
+    	genre.getItems().addAll(AnimeGenre.values());
     	
     }
     @FXML
-    private TextField nome,autor,episodes,score,genre,synopsis,link,data,seasons;
+    private ComboBox<AnimeGenre> genre;
 
     @FXML
-    void CarregarImagem(ActionEvent event) {	// botão para procurar a imagem no pc
+    private TextField nome,autor,episodes,score,synopsis,link,seasons;
+    @FXML
+    private DatePicker data;
+    
 
+	@FXML 
+    void CarregarImagem(ActionEvent event) {      // botão para procurar a imagem no pc
+
+		FileChooser fileChooser = new FileChooser();
+        
+        //Set extension filter
+        FileChooser.ExtensionFilter extFilterPNG = 
+                new FileChooser.ExtensionFilter("PNG files (*.PNG)", "*.PNG");
+        FileChooser.ExtensionFilter extFilterpng = 
+                new FileChooser.ExtensionFilter("png files (*.png)", "*.png");
+        fileChooser.getExtensionFilters()
+                .addAll(extFilterPNG, extFilterpng);
+
+        //Show open file dialog
+        File file = fileChooser.showOpenDialog(null);
+        
+        if (file != null) {
+        	try {
+            	bufferedImage = ImageIO.read(file);
+            } catch (IOException ex) {	
+            }
+        }
     }
-
     @FXML
     void Confirmar(ActionEvent event) {  // botão para confimar o upload da imagem do anime
     	
     	if(!nome.getText().isEmpty() && !autor.getText().isEmpty() && !link.getText().isEmpty() &&
-    			!data.getText().isEmpty() && !score.getText().isEmpty()&&  !episodes.getText().isEmpty() &&
-    			!genre.getText().isEmpty() && !synopsis.getText().isEmpty()&& !seasons.getText().isEmpty()) { 
-    		MIDAO.addAnime(nome, autor, data, score, episodes, seasons, genre, synopsis, link);
+    			data.getValue()!=null && !score.getText().isEmpty()&&  !episodes.getText().isEmpty() &&
+    			genre.getValue()!=null && !synopsis.getText().isEmpty()&& !seasons.getText().isEmpty()) { 
+			try {
+				//Vai converter a imagem que o utilizador fez upload para uma byte array
+				ByteArrayOutputStream baos = new ByteArrayOutputStream();
+				ImageIO.write(bufferedImage, "png", baos);
+				baos.flush();
+				byte[] imageInByte = baos.toByteArray();
+				baos.close();
+				//Vai adicionar o jogo na base de dados
+				MIDAO.addAnime(nome.getText(), autor.getText(), data.getValue().toString(),Float.valueOf(score.getText()),
+	    				Integer.valueOf(episodes.getText()),Integer.valueOf(seasons.getText()),String.valueOf(genre.getSelectionModel().getSelectedItem()), 
+	    				synopsis.getText(), link.getText(),imageInByte);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+    		
     		Dialog.warningDialog("", "Anime adicionado com sucesso!", "Anime Adicionado" );
-    		/*
-    		if (imagemJogo.getImage() != null && !nomeJogo.getText().isEmpty() && !categoriaJogo.getSelectionModel().isEmpty() 
-    				&& !devJogo.getText().isEmpty() && dataJogo.getValue() != null)*/
+    		
     	}
     	else {
 			Dialog.warningDialog("", "Preencha todos os campos anteriores!", "Atenção!" );
--- Animex/src/pt/iade/animex/models/daos/MIDAO.java
+++ Animex/src/pt/iade/animex/models/daos/MIDAO.java
@@ -1,24 +1,35 @@
 package pt.iade.animex.models.daos;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+
 
 public class MIDAO {
 	public static void addAnime(String nome, String autor,String data, float score,int episodes,int seasons,String genre, String synopsis,String link, byte[] imagem) {
+		InputStream imagem1 = new ByteArrayInputStream(imagem);
 		try {
-			PreparedStatement statement = DBConnector.getConnection().prepareStatement("INSERT INTO anime (nome, autor,data,score,episodes,seasons,genre,synopsis,link,imagem) values (?,?,?,?,?,?,?,?,?,?)");
+			//Vai verificar se o nome do jogo já existe na base de dados
+			PreparedStatement statement = DBConnector.getConnection().prepareStatement("SELECT * FROM animes WHERE nome=?");
 			statement.setString(1, nome);
-			statement.setString(2, autor);
-			statement.setString(3, data);
-			statement.setFloat(4, score);
-			statement.setInt(5, episodes);
-			statement.setInt(6, seasons);
-			statement.setString(7, genre);
-			statement.setString(8, synopsis);
-			statement.setString(9, link);
-			statement.setBytes(10, imagem);
-			statement.executeUpdate();
-
+			ResultSet results = statement.executeQuery();
+			if (!results.next()) {
+				//se nao existir vai adiconar na base de dados
+				PreparedStatement statement1 = DBConnector.getConnection().prepareStatement("INSERT INTO animes (nome, autor,data,score,episodes,seasons,genre,synopsis,link,imagem) values (?,?,?,?,?,?,?,?,?,?)");
+				statement1.setString(1, nome);
+				statement1.setString(2, autor);
+				statement1.setString(3, data);
+				statement1.setFloat(4, score);
+				statement1.setInt(5, episodes);
+				statement1.setInt(6, seasons);
+				statement1.setString(7, genre);
+				statement1.setString(8, synopsis);
+				statement1.setString(9, link);
+				statement1.setBinaryStream(10, imagem1);
+				statement1.executeUpdate();
+			}
 		}
 		catch (SQLException e) {
 			e.printStackTrace();
